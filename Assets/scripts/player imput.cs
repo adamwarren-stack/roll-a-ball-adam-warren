d86fee7880a0cfcf332e918894e145210a4637b1
adamwarren-stack <adam.warren@discoverystudent.org>
2025-10-30 11:35:30 -0400

    yw

---
+++ Assets/scripts/player imput.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine. InputSystem;
+
+public class playercontroller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMove(InputValue movementvalue)
+}
